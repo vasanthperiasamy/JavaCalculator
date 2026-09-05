4e78bec420f9f5f2be7e16a7f121ee96231a9255
vasanthperiasamy <vpaibuilder@gmail.com>
2026-09-06 02:22:18 +0530

    Calculator class created.Add addition feature to calculator

---
+++ src/main/java/src/com/myproject/Calculator.java
@@ -0,0 +1,13 @@
+package src.com.myproject;
+
+public class Calculator {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+    System.out.println("Addition: " + add(5, 5));
+  }
+
+  public static int add(int a, int b) {
+    return a + b;
+  }
+}
