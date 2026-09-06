238beab2ed2e415503859a93cfaffcf4c439eca3
vasanthperiasamy <vpaibuilder@gmail.com>
2026-09-06 14:27:22 +0530

    Feature multiplication (#1)
    * Add multiplication feature
    
    * Add multiplication feature

--- src/main/java/com/myproj/calculate/Calculator.java
+++ src/main/java/com/myproj/calculate/Calculator.java
@@ -5,9 +5,15 @@ public class Calculator {
   public static void main(String[] args) {
     // TODO Auto-generated method stub
     System.out.println("Addition: " + add(5, 5));
+    System.out.println("Multiply: " + multiply(5, 5));
   }
-  //comment added to simulate pull
+
+  // comment added to simulate pull
   public static int add(int a, int b) {
     return a + b;
+  }
+
+  public static int multiply(int a, int b) {
+    return a * b;
   }
 }
